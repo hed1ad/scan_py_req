9885bfec2a0e310c5b41ad70ca3c391c82b35869
hed1ad <diad.hedi@gmail.com>
2025-06-29 00:27:16 +0300

    dev

--- vuln_checker/api_client.py
+++ vuln_checker/api_client.py
@@ -1,14 +1,18 @@
 import requests
 from parser import parse_requirements
 
-url = "https://api.osv.dev/v1/query"
-data = parse_requirements("requirements.txt")
+def req_api():
+    requirements = parse_requirements("requirements.txt")
+    url = "https://api.osv.dev/v1/query"
+    
 
-for entry in data:
-    response = requests.post(url, json=entry)
-    print(f"Package: {entry['package']['name']}, Status: {response.status_code}")
-    try:
-        print(response.json())
-    except requests.exceptions.JSONDecodeError:
-        print("Response is not valid JSON:", response.text)
+    for entry in requirements:
+        response = requests.post(url, json=entry)
+        print(f"Package: {entry['package']['name']}, Status: {response.status_code}")
+        try:
+            print(response.json())
+        except requests.exceptions.JSONDecodeError:
+            print("Response is not valid JSON:", response.text)
 
+if __name__ == "__main__":
+    req_api()
